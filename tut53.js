3b3d700155911058652ed35a033eda7b0e938f06
rahul kumar <guptarahul6731@gmail.com>
2022-09-24 16:26:08 +0530

    first commit

---
+++ tut53.js
@@ -0,0 +1,39 @@
+console.log("this is tutorials 53")
+
+
+ //function greet(name){
+    function greet(name, greettext){
+        console.log(greettext + " " + name)
+    console.log(name + " is a good boy");
+}
+
+let name = "rahul";
+let name1 = "kaira";
+let name2 = "pagal";
+let name3 = "bandar";
+let greettext = "good morning"
+greet(name, greettext);
+greet(name1, greettext);
+greet(name2, greettext);
+greet(name3, greettext);
+
+
+
+
+/*
+console.log(name + " is a good boy");
+console.log(name1 + " is a good boy");
+console.log(name2 + " is a good boy");
+console.log(name3 + " is a good boy");
+*/
+
+
+// return value //
+function sum(a, b, c){
+    let d = a + b + c;
+    return d;
+
+}
+
+let returnval = sum(1, 82, 3);
+console.log(returnval);
---
+++ tut64.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+const text = fs.readFileSycn("dele.txt", "utf-8");
+console.log("the content of the file is ")
+console.log(text);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
